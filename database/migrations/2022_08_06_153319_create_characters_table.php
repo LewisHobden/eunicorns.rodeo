5af5d92cbdda7d7896bc805f02e50e32166472e2
Lewis Hobden <lewis@hobden.xyz>
2022-08-14 20:20:58 +0100

    Updated build rules and HTTPS

--- database/migrations/2022_08_06_153319_create_characters_table.php
+++ database/migrations/2022_08_06_153319_create_characters_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
 
             $table->unsignedBigInteger("user_id");
             $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
-            $table->text("in_game_name")->unique();
+            $table->char("in_game_name",255)->unique();
             $table->text("class");
             $table->integer("item_level");
         });
